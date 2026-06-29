7c3f20c5d7ce717af7b331b55c638b4fa435f731
Bele Zbrun <zbrunbelu@gmail.com>
2026-06-29 17:24:30 -0300

    Agregue la direccion de NestJS a una variable de entorno

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Una "instancia" de axios que ya sabe dónde está el backend
 const api = axios.create({
-  baseURL: 'http://localhost:3000', // La dirección de tu NestJS
+  baseURL: import.meta.env.VITE_API_URL, // La dirección de tu NestJS
 });
 
 export default api;
