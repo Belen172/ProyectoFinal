a6c441b32bc6dc107feddc857415b62c03e1b8df
Bele Zbrun <zbrunbelu@gmail.com>
2026-06-29 17:55:59 -0300

    Modifique el CORS en main agregando la url del frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Habilita CORS para que el frontend pueda comunicarse con el backend
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://proyecto-bike.vercel.app', // La URL exacta de tu frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
